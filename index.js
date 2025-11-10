e19fe6d61a9d06b98c44a5d2cda8660ab97ea93e
Wasim Ansari <oasimansari0786@gmail.com>
2025-11-10 16:16:23 +0530

    create delete route

--- index.js
+++ index.js
@@ -73,6 +73,13 @@ app.get("/posts/:id/edit" , (req, res) =>{
   // res.send("hello edit")
 })
 
+// DELETE
+app.delete("/posts/:id", (req, res)=>{
+  let {id} = req.params;
+  posts = posts.filter((p)=> id !== p.id);
+  res.redirect("/posts");
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
